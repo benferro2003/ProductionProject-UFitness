60fcd889d10366c8f9e76b2e975ecda08b8e0fce
benferro2003 <benferro2003@gmail.com>
2025-02-12 18:23:47 +0000

    created layout page component

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</head>
+<header>
+    @include('Navbar')
+</header>
+<body>
+    {{$slot}}
+</body>
+</html>
