9f25fe42590b53ed097f0dc7afb85ded315be494
benferro2003 <benferro2003@gmail.com>
2025-02-20 21:14:42 +0000

    Correct comment formatting

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <title>Home Page</title>
-    <!-->bootstrap stylesheet used</-->
+    <!-- bootstrap stylesheet used -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-     <!-->bootstrap javascript used</-->
+     <!-- bootstrap javascript used -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </head>
 <header>
