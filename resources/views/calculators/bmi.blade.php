4e3f36bb10a66cd6ca6e02391839a4d441c3e5f9
benferro2003 <benferro2003@gmail.com>
2025-03-05 15:46:54 +0000

    updated bmi form to just weight and age updated form action to correct controller method output displayed:    check the bmi to output standards

--- resources/views/calculators/bmi.blade.php
+++ resources/views/calculators/bmi.blade.php
@@ -1,44 +1,61 @@
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<header>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    crossorigin="anonymous"></script>
-  @include('Navbar')
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    @include('Navbar')
+    <title>BMI</title>
+</head>
 
 <body class="background">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h1 class="text-center">BMI Calculator</h1>
+        <h1 class="text-center">BMI Calculator 18+</h1>
       </div>
     </div>
     <div class="row">
       <div class="col-md-6 offset-md-3">
-        <form action="POST" method="POST">
+        <form action="{{ route('calculator.calculate', 'bmi') }}" method="POST">
           @csrf
-          <label for="Age">Age</label><br>
-          <input type="text" name="age" id="age" class="form-control">
-
-          <div class="form-group">
-            <label for="sex">Sex</label>
-            <select class="form-control" id="sex">
-              <option></option>
-              <option value = "male">Male</option>
-              <option value = "female">Female</option>
-            </select>
-          </div>
-
+          
             <label for="Weight">Weight (In kg)</label>
-            <input type="text" name="weight" id="weight" class="form-control">
+            <input type="text" name="weight" id="weight" class="form-control" required>
 
             <label for="Height">Height (In cm)</label>
-            <input type="text" name="height" id="height" class="form-control">
+            <input type="text" name="height" id="height" class="form-control" required>
 
 
            <br>
 
           <button type="submit" class="btn btn-primary">Calculate</button>
         </form>
+        @if(session('result'))
+          @php
+            $bmi_value = "";
+            if(session('result') < 18.5){
+              $bmi_value = "Underweight";
+            }
+            elseif(session('result') >= 18.5 && session('result') < 24.9){
+              $bmi_value = "Normal weight";
+            }
+            elseif(session('result') >= 25 && session('result') < 29.9){
+              $bmi_value = "Overweight";
+            }
+            elseif(session('result') >= 30){
+              $bmi_value = "Obese";
+            }
+          @endphp
+
+          <div class="alert alert-success mt-3">
+              Your estimated BMI is {{ session('result') }} - {{$bmi_value}}<br>
+          </div>
+        @endif
+            </div>
+        </div>
+    </div>
 </body>
+</html>
