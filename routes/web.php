19a25803807f29b1a44b3f96e4c5bf527165c4ed
benferro2003 <benferro2003@gmail.com>
2025-03-05 14:32:38 +0000

    modified web.php -added one get route for ALL calculators -added post route for calculator calculate controller method -added more comments

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CalculatorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
+//Dashboard route
 Route::get('/dashboard', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/CalorieCalculator', function () {
-    return view('CalorieCalculator');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Calculator routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/calculator/{type}', [CalculatorController::class, 'show'])->name('calculator.show');
+    //Route for calculating the calories
+    Route::post('/calculator/{type}/calculate', [CalculatorController::class, 'calculate'])->name('calculator.calculate');
 
-Route::get('/One_rep_Calculator', function () {
-    return view('One_rep_Calculator');
-})->middleware(['auth', 'verified'])->name('dashboard');
+});
 
-Route::get('/BMI_Calculator', function () {
-    return view('BMI_Calculator');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+//Profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
