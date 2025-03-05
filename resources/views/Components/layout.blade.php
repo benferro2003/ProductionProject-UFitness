d5607d19a2dd5d9360afe9c33578c504d199e4e9
benferro2003 <benferro2003@gmail.com>
2025-03-05 15:44:21 +0000

    updated formatting in layout blade

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -2,15 +2,21 @@
 <html lang="en">
 <head>
     <title>Home Page</title>
-    <!-- bootstrap stylesheet used -->
+    <!-- Bootstrap stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-     <!-- bootstrap javascript used -->
+    <!-- Custom stylesheet -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </head>
-<header>
-    @include('Navbar')
-</header>
-<body class = "background">
-    {{$slot}}
+
+<body class="background">
+    <header>
+        @include('Navbar')
+    </header>
+
+    <main>
+        {{$slot}}
+    </main>
 </body>
 </html>
