d0d3a9452da1c54db4c85991cd8f14b1fcbcd4cc
benferro2003 <benferro2003@gmail.com>
2025-03-09 11:55:03 +0000

    added placeholders for graphs: updated app.css updated home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,15 @@
     
     <header>
         <h1 class="Title display-4">Welcome to UFIT</h1>
-    </header>  
+    </header> 
+    <div class = "graph-workouts">
+        <p class = "section-title">Workouts Per Week</p>
+        <img src="{{ asset('graph.png') }}" width="500" height="500">
+    <div class
+    <div class = "graph-goals">
+        <p class = "section-title">Weight progress</p>
+        <img src="{{ asset('graph.png') }}" width="500" height="500">
+    <div class
 </x-layout>
 </body>
 </html>
