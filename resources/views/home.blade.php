a70c2d29075713e97b77539e5f49e3a634d8a98c
benferro2003 <benferro2003@gmail.com>
2025-03-10 18:22:02 +0000

    updated css to be more suitable: darker blue, more appealing
    Title is more welcoming
    
    containers arranged in aesthetic way.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,30 @@
 <x-layout>
     
     <header>
-        <h1 class="Title display-4">Welcome to UFIT</h1>
+        <h1 class="Title">Welcome to <br> <img src="{{ asset('Dumbbell.png') }}" width="500" height="300" alt="Logo" class="navbar-brand"> <br> UniversalFit</h1>
     </header> 
-    <div class = "graph-workouts">
-        <p class = "section-title">Workouts Per Week</p>
-        <img src="{{ asset('graph.png') }}" width="500" height="500">
-    <div class
-    <div class = "graph-goals">
-        <p class = "section-title">Weight progress</p>
-        <img src="{{ asset('graph.png') }}" width="500" height="500">
-    <div class
+
+
+
+    <main class>
+        <div class = "about-container">
+            <p class = "section-title">About us?</p>
+            <p> UFIT is a fitness application that allows users to track their workouts, set goals, and monitor their progress. 
+                The application also provides users with a variety of tools such as calculators and workout generators to help them achieve their fitness goals. 
+                Whether you are a beginner looking to get started or an experienced athlete looking to take your training to the next level, UFIT has something for everyone.</p>
+        </div>
+
+        <div class = "graph-container">
+            <div class = "graph-workouts">
+                <p class = "section-title">Workouts Per Week</p>
+                <img src="{{ asset('graph.png') }}" class = "graph">
+            </div>
+            <div class = "graph-goals">
+                <p class = "section-title">Weight progress</p>
+                <img src="{{ asset('graph.png') }}" class = "graph">
+            </div>
+        </div>
+    </main>
 </x-layout>
 </body>
 </html>
