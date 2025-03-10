f2722625776ff1a9808beceff4c9be9f125e33f0
benferro2003 <benferro2003@gmail.com>
2025-03-10 18:22:32 +0000

    updated navbar elements to white to handle new css

--- resources/views/Navbar.blade.php
+++ resources/views/Navbar.blade.php
@@ -1,20 +1,20 @@
 <!-- Navbar - code from bootstrap -->
 <!-- link - https://getbootstrap.com/docs/4.0/components/navbar/ -->
-<nav class="navbar navbar-expand-lg custom-nav">
+<nav class="navbar navbar-expand-lg custom-nav" >
   <div class="container-fluid">
     <!--Show logo in Nav Bar -->
     <img src="{{ asset('Dumbbell.png') }}" width="100" height="100" alt="Logo" class="navbar-brand">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+    <button style="background-color: white;" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
       aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" style = "color: white" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" style = "color: white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Calculators
           </a>
           <!--Drop down menu for calculators-->
@@ -27,7 +27,7 @@
         </li>
 
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" style = "color: white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Workouts
           </a>
           <ul class="dropdown-menu">
@@ -42,7 +42,7 @@
       <ul class="navbar-nav ms-auto">
         @auth
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" style = "color: white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="{{ asset('Dumbbell.png') }}" alt="Profile" width="40" height="40" class="rounded-circle">
         {{ Auth::user()->name }}
         </a>
@@ -60,10 +60,10 @@
       </li>
     @else
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('login') }}">Login</a>
+      <a class="nav-link" style = "color: white" href="{{ route('login') }}">Login</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('register') }}">Register</a>
+      <a class="nav-link" style = "color: white" href="{{ route('register') }}">Register</a>
     </li>
   @endauth
       </ul>
