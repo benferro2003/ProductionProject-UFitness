dd1ac30bf432e50c0e4941b44481c84665ec57dd
benferro2003 <benferro2003@gmail.com>
2025-03-11 15:36:12 +0000

    created workout controller for workouts functions such as: Generating workouts Showing Workout Files Outputting result of generator

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class WorkoutController extends Controller
 {
-    //
+    public function show()
+    {
+        //return the associated calculator view
+        return view('workouts.create');
+    }
+
 }
