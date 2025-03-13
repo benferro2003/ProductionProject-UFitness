8e64d8ede46ebec10ec3c4b67d45cf74df0200d0
benferro2003 <benferro2003@gmail.com>
2025-03-13 18:11:41 +0000

    updated form to generate workout with correct vals matching api requested input

--- resources/views/workouts/create.blade.php
+++ resources/views/workouts/create.blade.php
@@ -13,32 +13,44 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="text-center">Workout Generator</h1>
+                <h1 class="Title">Generate a Workout</h1>
             </div>
         </div>
-        <div class="row">
+        <div class="form-container">
             <div class="col-md-6 offset-md-3">
                 <form action="{{ route('generate.workout') }}" method="POST">
                     @csrf
                    
                    
                     <div class="form-group">
-                    <label for="available_days">Available Days:</label>
-                    <select name="available_days" id="available_days" class="form-control">
-                    </select>
-                    </div>
+                    <label for="available_days">Available Days:</label><br>
+                    <!-- radio form allowing multiple days to be selected -->
+                    <input type="checkbox" name="available_days[]" value="Mon"> Monday<br>
+                    <input type="checkbox" name="available_days[]" value="Tue"> Tuesday<br>
+                    <input type="checkbox" name="available_days[]" value="Wed"> Wednesday<br>
+                    <input type="checkbox" name="available_days[]" value="Thu"> Thursday<br>
+                    <input type="checkbox" name="available_days[]" value="Fri"> Friday<br>
+                    <input type="checkbox" name="available_days[]" value="Sat"> Saturday<br>
+                    <input type="checkbox" name="available_days[]" value="Sun"> Sunday<br>
+                    </div><br>
 
                     <div class="form-group">
-                    <label for="equipment">Available Equipment:</label>
-                    <select name="equipment" id="equipment" class="form-control">
-                    </select>
-                    </div>
+                    <label for="equipment">Available Equipment:</label><br>
+                    <input type="checkbox" name="equipment[]" value="dumbbell"> Dumbbell<br>
+                    <input type="checkbox" name="equipment[]" value="barbell"> Barbell<br>
+                    <input type="checkbox" name="equipment[]" value="kettlebell"> Kettlebell<br>
+                    <input type="checkbox" name="equipment[]" value="body weight"> BodyWeight<br>
+                    <input type="checkbox" name="equipment[]" value="cable"> Cables<br>
+                    </div><br>
 
                     <div class="form-group">
-                    <label for="fitness_level">Fitness Level:</label>
-                    <select name="fitness_level" id="fitness_level" class="form-control">
+                    <label for="fitness_level">Fitness Level:</label required><br>
+                    <select name="fitness_level" id="fitness_level">
+                        <option value="Beginner">Beginner</option><br>
+                        <option value="Intermediate">Intermediate</option><br>
+                        <option value="Advanced">Advanced</option><br>
                     </select>
-                    </div>
+                    </div><br>
 
                     <button type="submit" class="btn btn-primary">Generate</button>
                 </form>
