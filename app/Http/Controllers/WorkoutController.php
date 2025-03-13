e4dc815cbf4c093c4ee52392ba9f0413003fcd76
benferro2003 <benferro2003@gmail.com>
2025-03-13 18:13:17 +0000

    created generate method in workout controller:
    sends api request to return exercises
    based on equipment
    need to add more logic to generate workouts for user
    based on fitness level & available days etc.

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -12,4 +12,59 @@ class WorkoutController extends Controller
         return view('workouts.create');
     }
 
+    public function generate(Request $request)
+{
+    // Validate the request
+    $validatedData = $request->validate([
+        'available_days' => 'required|array',
+        'equipment' => 'required|array',
+        'fitness_level' => 'required|string',
+    ]);
+
+    // Get the list of equipment
+    $equipmentList = $validatedData['equipment'];
+
+    // create empty array to store workout data
+    $workoutData = [];
+
+    //can only send one equipment at a time so loop through each equipment in the list
+    foreach ($equipmentList as $equipment) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://exercisedb.p.rapidapi.com/exercises/equipment/$equipment",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "x-rapidapi-host: exercisedb.p.rapidapi.com",
+                "x-rapidapi-key: " . env('EXERCISE_DB_API_KEY')
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        //decode json
+        $decodedResponse = json_decode($response, true);
+
+        // Ensure response is valid
+        if (is_array($decodedResponse)) {
+            $workoutData = array_merge($workoutData, $decodedResponse);
+        }
+    }
+
+    //return workout result view
+    return view('workouts.result', [
+        'workoutPlan' => $validatedData,
+        'workoutData' => $workoutData
+    ]);
 }
+
+}
+
+//currently only returns exercises based on equipment, need to add more logic to generate a workout plan
+//for the user based on their fitness level and available days
