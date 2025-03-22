538ddc5390657108bb6e66d8fd6d0cce9041e390
benferro2003 <benferro2003@gmail.com>
2025-03-22 15:28:53 +0000

    Modified styling in Result page
    Added note to reload page to shuffle exercises
    
    --> need to implement button that does this

--- resources/views/workouts/result.blade.php
+++ resources/views/workouts/result.blade.php
@@ -64,25 +64,27 @@
 
     </div><br><br>
 
-    <h1 class="Title m-10">Generated Workout Plan:</h1>
+    <h1 style="padding-bottom:100px">**RELOAD PAGE TO SHUFFLE EXERCISES**</h1>
+
+    <h1 class="Title m-150">Generated Workout Plan:</h1>
     <div class="workout-container">
         @if(is_array($workoutData) && count($workoutData) > 0)
             @foreach($workoutData as $day => $exercises)
                 <div>
-                    <h2>{{ $day }}</h2>
+                    <h2 class = "Title" style="align-content:left; margin-top: 50px;"><ul>{{ $day }}</ul></h2>
                     <div class = "row"> 
                         @foreach($exercises as $exercise)
                             <div class="col-md-4">
-                                <div class="card mb-3">
+                                <div class="card mb-3" style="height:1300px; color: #34495e">
                                     <div class="card-body">
-                                        <h2 class="card-title" style="font-weight:bold">{{ $exercise['name'] }}</h2><br>
+                                        <h2 class="card-title mt-20" style="font-weight:bold">{{ $exercise['name'] }}</h2><br>
                                         <p class ="card-text"><strong>Sets:</strong> {{ $sets }}</p>
                                         <p class ="card-text"><strong>Reps:</strong> {{ $reps }}</p>
                                         <p class="card-text"><strong>Body Part:</strong> {{ $exercise['bodyPart'] }}</p>
                                         <p class="card-text"><strong>Target Muscle:</strong> {{ $exercise['target'] }}</p>
                                         <p class="card-text"><strong>Equipment:</strong> {{ $exercise['equipment'] }}</p>
                                         <p class="card-text"><strong>Instructions:</strong></p>
-                                        <img src="{{ $exercise['gifUrl'] }}" class="card-img" alt="Exercise image">
+                                        <img src="{{ $exercise['gifUrl'] }}" class="card-img" style="width:fit-content; height: fit-content;" alt="Exercise image">
                                         <ul>
                                             @foreach($exercise['instructions'] as $instruction)
                                                 <li>{{ $instruction }}</li>
@@ -100,8 +102,5 @@
             <p>No workout plan available.</p>
         @endif
     </div>
-
-
-
 </body>
 </html>
