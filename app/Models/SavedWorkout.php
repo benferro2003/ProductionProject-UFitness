875b53d573a4c3004045ee1b5136de616897d966
benferro2003 <benferro2003@gmail.com>
2025-03-25 20:39:59 +0000

    Created saved_workouts db Table with model file -SavedWorkout, to allow workout plan json to be stores in db if user decides to save workout

---
+++ app/Models/SavedWorkout.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class SavedWorkout extends Model
+{
+    use HasFactory;
+
+    protected $table = 'saved_workouts'; 
+    protected $fillable = ['user_id', 'workout_plan']; 
+}
