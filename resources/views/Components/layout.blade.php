c961b72fa678fb914d930b683996a14da6f1edde
benferro2003 <benferro2003@gmail.com>
2025-03-31 10:30:42 +0100

    removed navbar from layout to see homepage

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -2,19 +2,13 @@
 <html lang="en">
 <head>
     <title>Home Page</title>
-    <!-- Bootstrap stylesheet -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <!-- Custom stylesheet -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </head>
-
 <body class="background">
-    <header>
-        @include('Navbar')
-    </header>
-
     <main>
         {{$slot}}
     </main>
