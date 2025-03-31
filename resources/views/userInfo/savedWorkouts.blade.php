62ed64e724a5c48960cacc6e503f9c9b8c2428fd
benferro2003 <benferro2003@gmail.com>
2025-03-31 10:36:33 +0100

    SavedWorkouts correctly displaying:
    decode workout storing as plan
    created workout id($workoutID)
    created day id ($DAY)
    
    For each saved workout:
    - Display workout - date created
    -Accordian from result blade used with new workout id to display
    
    ->Collapsible content with each days related exercises.
    
    ->currently exercise img from
    api expires after 12 hrs
    --> on error display placeholder svg
    ----> need to fix to replace imgs

--- resources/views/userInfo/savedWorkouts.blade.php
+++ resources/views/userInfo/savedWorkouts.blade.php
@@ -15,15 +15,80 @@
 
 <body class="background">
     <div class="container mt-5">
-        <h1 class="Title">My Workouts</h1>
+        <h1 class="Title">
+            My Plans
+        </h1>
+
         @if(count($savedWorkouts) > 0)
-            <!-- need to output contents of savedWorkouts -->  
-            number of saved workouts:
-            @php
-                echo count($savedWorkouts);
-                echo "<br>saved workout pure data:";
-                dd($savedWorkouts);
-            @endphp
+            @foreach($savedWorkouts as $workout)
+                @php
+                    $plan = json_decode($workout->workout_plan, true);
+                    $workoutID = str_replace('-', '', $workout->created_at->format('d-m-y'));
+                @endphp
+
+                <div class="mb-5 p-3 border rounded mt-20">
+                    <h1>Workout - {{ $workout->created_at->format('d-m-y') }}</h1>
+                    <div class=accordion>
+                @php 
+                    //counter for day index
+                    $dayind = 0; 
+                @endphp
+                @foreach($plan as $day => $exercises)
+                    @php 
+                        //Create unique ID for each day in this workout
+                        $DAY = 'day' . $workoutID . '_' . $dayind; 
+                    @endphp
+                    <div class=accordion-item>
+                        <!-- Use day index to create unique id for each day -->
+                        <h2 class="accordion-header" id="heading{{ $DAY }}">
+                            <!-- use bootstrap accordion-button setting it as closed with collapsable elements-->
+                            <button class="accordion-button collapsed" 
+                                type="button" 
+                                data-bs-toggle="collapse"
+                                data-bs-target="#collapse{{ $DAY }}" aria-expanded="false" aria-controls="collapse{{ $DAY }}">
+                                <!-- display day -->
+                                {{ $day }}
+                            </button>
+                        </h2>
+                        <!-- Collapsible content with each days related exercises  -->
+                        <div id="collapse{{ $DAY }}" class="accordion-collapse collapse" aria-labelledby="heading{{ $DAY }}"
+                            data-bs-parent="#workoutAccordion{{ $workoutID }}">
+                            <div class="accordion-body">
+                                <div class="row">
+                                    @foreach($exercises as $exercise)
+                                        <div class="col-md-4">
+                                            <div class="card mb-3" style="height:1300px; color: #34495e">
+                                                <div class="card-body">
+                                                    <h2 class="card-title mt-20" style="font-weight:bold">{{ $exercise['name'] }}
+                                                    </h2><br>
+                                                    <p class="card-text"><strong>Body Part:</strong> {{ $exercise['bodyPart'] }}</p>
+                                                    <p class="card-text"><strong>Target Muscle:</strong> {{ $exercise['target'] }}
+                                                    </p>
+                                                    <p class="card-text"><strong>Equipment:</strong> {{ $exercise['equipment'] }}
+                                                    </p>
+                                                    <p class="card-text"><strong>Instructions:</strong></p>
+                                                    <img src="{{ $exercise['gifUrl'] }}" class="img-fluid exercise-img"
+                                                    alt="Exercise image"
+                                                    onerror="this.src='{{ asset('images/no-exercise.svg') }}'">
+                                                    <ul>
+                                                        @foreach($exercise['instructions'] as $instruction)
+                                                            <li>{{ $instruction }}</li>
+                                                        @endforeach
+                                                    </ul>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @php $dayind++; @endphp
+                @endforeach
+        </div>
+                </div>
+            @endforeach
         @else
             <div class="text-center">
                 <p class="lead">You don't have any saved workouts yet.</p>
