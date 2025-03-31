9a09fb8a1d47c3e669b927d2a4b267d0229e902d
benferro2003 <benferro2003@gmail.com>
2025-03-31 10:30:14 +0100

    added left and right class to graphs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<header>
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Home - UFIT</title>
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        @include('Navbar')
+        <h1 class="Title">Welcome to <br> <img src="{{ asset('Dumbbell.png') }}" width="100" height="100" alt="Logo" class="navbar-brand"> <br> UniversalFit
+        </h1>
+    </header> 
 <body>
 <x-layout>
-    
-    <header>
-        <h1 class="Title">Welcome to <br> <img src="{{ asset('Dumbbell.png') }}" width="500" height="300" alt="Logo" class="navbar-brand"> <br> UniversalFit</h1>
-    </header> 
-
-
-
     <main class>
-        <div class = "about-container">
+        <div class = "centre mt-5">
             <p class = "section-title">About us?</p>
             <p> UFIT is a fitness application that allows users to track their workouts, set goals, and monitor their progress. 
                 The application also provides users with a variety of tools such as calculators and workout generators to help them achieve their fitness goals. 
@@ -22,14 +20,14 @@
         </div>
 
         <div class = "wrapped-containers">
-            <div class = "graph-workouts">
+            <div class = "left">
                 <p class = "section-title">Workouts Per Week</p>
                 <p class = "section-title">workout progress placeholder</p>
                 <img src="{{ asset('graph.png') }}"class = "img-fluid"> 
                 
 
             </div>
-            <div class = "graph-goals">
+            <div class = "right">
                 <p class = "section-title">Weight progress</p>
                 <p class = "section-title">weight progress placeholder</p>
                 <img src="{{ asset('graph.png') }}"class = "img-fluid"> 
