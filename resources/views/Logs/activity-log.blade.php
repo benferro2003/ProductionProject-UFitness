aeaed08d3f2c4da565d004775086ef586068d385
benferro2003 <benferro2003@gmail.com>
2025-04-02 18:59:56 +0100

    Added forms from yesterdays progress:
    form created for activity log
    
    form created for weight log

--- resources/views/Logs/activity-log.blade.php
+++ resources/views/Logs/activity-log.blade.php
@@ -1,26 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-<header>
+
+<head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
     @include('Navbar')
-    <h1 class="Title">
-        Weight Log
-    </h1>
-</header>
+    <title>Log Workout</title>
+    <h1 class="Title">LOG WORKOUT</h1>
+</head>
 
 <body class="background">
+    <div class="card mt-5"
+        style="background-color: #34495e; color: white; border-radius: 10px;width: 90%;margin: auto;">
+        <form action="{{ route('log.workout') }}" method="POST">
+            @csrf
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="form-control-lg">
+                        <label class="form-label fw-bold" for="workout_name">Workout Name :</label>
+                        <select class="form-select" name="workout_name" id="workout_name">
+                            <option value="cardio">Cardio</option>
+                            <option value="push">Push</option>
+                            <option value="pull">Pull</option>
+                            <option value="legs">Legs</option>
+                            <option value="full body">Full Body</option>
+                            <option value="upper">Upper Body</option>
+                            <option value="lower">Lower Body</option>
+                        </select>
+                    </div>
 
+                    <div class="form-control-lg">
+                        <label class = "form-label fw-bold" for="duration">Duration (In minutes) :</label>
+                        <input type="text" class="form-control" name="duration" id="duration" required>
+                    </div><br>
+
+                    <div class="text-center mt-4">
+                        <button type="submit" class="btn btn-outline-light btn-lg">Log Workout</button>
+                    </div>
+                </div>
+            </div><br>
+        </form>
+    </div><br><br>
 </body>
 
 
 <footer>
-    <div class = "footer">
-        <div class= "centre mt-5">
+    <div class="card" style="background-color: #34495e; color: white; border-radius: 10px;width: 90%;margin: auto;">
+        <div class="centre">
             <a href="{{ route('weightLog.show') }}" class="btn btn-outline-light btn-lg btn-block left">LOG WEIGHT</a>
         </div>
     </div>
--- resources/views/Logs/weight-log.blade.php
+++ resources/views/Logs/weight-log.blade.php
@@ -1,26 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<header>
+<head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
     @include('Navbar')
-    <h1 class="Title">
-        WORKOUT LOG
-    </h1>
-</header>
+    <title>Log Weight</title>
+    <h1 class="Title">LOG WEIGHT</h1>
+</head>
 
 <body class="background">
+    <div class="card mt-5"
+        style="background-color: #34495e; color: white; border-radius: 10px;width: 90%;margin: auto;">
+        <form action="{{ route('log.weight') }}" method="POST">
+            @csrf
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="form-control-lg">
+                        <label class="form-label fw-bold" for="weight">Weight (In kg) :</label>
+                        <input type="text" class="form-control" name="weight" id="weight" required>
+                    </div><br>
+                    <div class="form-control-lg">
+                        <label class="form-label fw-bold" for="weight_goal">Goal :</label>
+                        <select class="form-select" name="weight_goal" id="weight_goal">
+                            <option value="loss">Weight Loss</option>
+                            <option value="gain">Weight Gain</option>
+                            <option value="maintain">Maintain Weight</option>
+                        </select>
+                    </div>
+
+                    <div class="text-center mt-4">
+                        <button type="submit" class="btn btn-outline-light btn-lg">Log Weight</button>
+                    </div>
+                </div>
+            </div><br>
+        </form>
+    </div><br><br>
 </body>
 
+
 <footer>
-    <div class="footer">
-            <div class="centre mt-5">
-                <a href="{{ route('activityLog.show') }}" class="btn btn-outline-light btn-lg btn-block left">LOG WORKOUT</a>
-            </div> 
+    <div class="card"
+    style="background-color: #34495e; color: white; border-radius: 10px;width: 90%;margin: auto;">
+        <div class="centre">
+            <a href="{{ route('activityLog.show') }}" class="btn btn-outline-light btn-lg btn-block">LOG WORKOUT</a>
         </div>
     </div>
 </footer>
