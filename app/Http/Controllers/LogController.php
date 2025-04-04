b22d48f2389d771235e246c091371acdaae2e51f
benferro2003 <benferro2003@gmail.com>
2025-04-04 23:18:57 +0100

    created $workoutcount to return count of workouts to home page for each week(day start of week/ month) - sorted logged weight and logged workouts asc rather than desc for charts

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\WeightLog;
 use App\Models\ActivityLog;
 use Ramsey\Uuid\Type\Decimal;
+use Carbon\Carbon;
 
 class LogController extends Controller
 {
@@ -75,11 +76,26 @@ class LogController extends Controller
         if (!auth()->check()) {
             return redirect()->route('login')->with('error', 'You need to log in first.');
         }
-        $loggedWorkouts = auth()->user()->activityLogs()->orderBy('created_at', 'desc')->get();
-        $loggedWeights = auth()->user()->weightLogs()->orderBy('created_at', 'desc')->get();
+        $loggedWorkouts = auth()->user()->activityLogs()->orderBy('created_at', 'asc')->get();
+        $loggedWeights = auth()->user()->weightLogs()->orderBy('created_at', 'asc')->get();
+
+        //variable to group workouts by week
+        //return the count of workouts per week
+        //map the count of workouts to variable count
+        $workoutCount = $loggedWorkouts->groupBy(function ($item) {
+            return $item->created_at->startofWeek()->format('d/m');
+        })-> map (function ($group) {
+            return $group->count();
+        });
+
+        //view weight trend
+        //get weight of each day
+        //group by date
+
         return view('home', [
             'loggedWorkouts' => $loggedWorkouts,
             'loggedWeights' => $loggedWeights,
+            'workoutCount' => $workoutCount,
         ]);
     }
 
