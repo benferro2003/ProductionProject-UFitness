3f10747f3d36dd25f44c3514704f6077025118c7
benferro2003 <benferro2003@gmail.com>
2025-04-04 18:32:36 +0100

    changes from 3rd april:
    added tables to display logs
    
    added error messaging

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,19 @@
     </h1>
 </header>
 
-<body>
+<body class="background">
+    <!-- display success and error messages -->
+    @if(session('error'))
+        <div class="alert alert-danger text-center">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    @if(session('success'))
+        <div class="alert alert-success text-center">
+            {{ session('success') }}
+        </div>
+    @endif
     <x-layout>
         <main class>
             <div class="wrapped-containers">
@@ -30,10 +42,12 @@
                 <div class="right mt-5">
                     <!-- link to track activity / log weight -->
                     <p class="section-title"> Log Progress</p>
-                    <div class = wrapped-containers>
-                        
-                    <a href="{{ route('activityLog.show') }}" class="btn btn-outline-light btn-lg btn-block left">Log Workout</a>
-                    <a href="{{ route('weightLog.show') }}" class="btn btn-outline-light btn-lg btn-block right">Log Weight</a>
+                    <div class=wrapped-containers>
+
+                        <a href="{{ route('activityLog.show') }}"
+                            class="btn btn-outline-light btn-lg btn-block left">Log Workout</a>
+                        <a href="{{ route('weightLog.show') }}" class="btn btn-outline-light btn-lg btn-block right">Log
+                            Weight</a>
                     </div>
                 </div>
 
@@ -42,17 +56,61 @@
             <div class="wrapped-containers">
                 <div class="left mt-5">
                     <p class="section-title">Workouts Progress</p>
-                    <img src="{{ asset('graph.png') }}" class="img-fluid centre-graph">
-
-
+                    <div class="table-responsive">
+                        <table class="table table-dark centre">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Workout Name</th>
+                                    <th scope="col">Duration(Mins)</th>
+                                    <th scope="col">Date (D/M/Y)</th>
+                                    <th scope="col">Total (# workouts)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($loggedWorkouts as $workout)
+                                    <tr>
+                                        <td>{{ $workout->workout_name }}</td>
+                                        <td>{{ $workout->duration }}</td>
+                                        <td>{{ $workout->created_at->format('d/m/y') }}</td>
+                                        <td>{{ $workout->id}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
+
                 <div class="right mt-5">
                     <p class="section-title">Weight progress</p>
-                    <img src="{{ asset('graph.png') }}" class="img-fluid centre-graph">
+                    <div class="table-responsive">
+                        <table class="table table-dark centre">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Weight</th>
+                                    <th scope="col">Goal</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Progress</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($loggedWeights as $weight)
+                                    <tr>
+                                        <td>{{ $weight->weight }}</td>
+                                        <td>{{ $weight->goal }}</td>
+                                        <td>{{ $weight->created_at->format('d/m/y')}}</td>
+                                        <td>{{ $weight->id}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
+
         </main>
+
     </x-layout>
+
 </body>
 
 </html>
